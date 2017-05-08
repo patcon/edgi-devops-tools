3ace4c7ce40a8cb0c121d7743e86aae63b2b468c
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-05-08 03:04:16 -0400

    Clean up cookbook separation.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,13 @@
 current_dir = File.dirname(__FILE__)
-local_mode = true
 
-chef_repo_path = '..'
+#cookbook_path [
+#  './../edgi-cookbooks',
+#  './../community-cookbooks',
+#]
 
-cookbook_path = [
-  '../edgi-cookbooks',
-  '../community-cookbooks',
-]
+cookbook_path [File.expand_path('../../community-cookbooks' , __FILE__), File.expand_path('../../edgi-cookbooks' , __FILE__)]
+local_mode true
+
 
 knife['bootstrap_version'] = '12'
 knife['ssh_user'] = 'root'
